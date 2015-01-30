4cc5fe9c6e7f9f87ed4b55663fa680a436a7aba7
Ipizdetc <ipizdetc@gmail.com>
2015-01-30 15:19:47 +0300

    Initial demo app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '1721b0c01017e99c223afa7f94c6a3ebed991a8f6978586224f5fde70a1965ada3646ff87cf073feed25ea7fd2b036348730eced70450fd482514812ec8c9075'
